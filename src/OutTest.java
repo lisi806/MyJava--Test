9073d686739ad3a27e18958d628e4d9c3983b37f
Lcq <lisi806@163.com>
2013-04-15 15:43:31 +0800

    add

--- src/OutTest.java
+++ src/OutTest.java
@@ -8,21 +8,10 @@ public class OutTest implements Cloneable {
 	 * @param args
 	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		OutTest out = new OutTest();
-		out.aa = "aa";
-		OutTest t1 = null;
-		try {
-			t1 = (OutTest) out.clone();
-			t1.aa = "bb";
-			System.out.println("t1:"+t1.aa);
-		} catch (CloneNotSupportedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(out);
-		System.out.println(t1);
-		
+	public static void main(String[] args) {
+		String aa = "aaaabbb";
+		aa.replace("ab", "xx");
+		System.out.println(aa);
 	}
 
 }
