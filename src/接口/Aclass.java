f54e48b74af1c0311cda0331674c28476569c2b8
LiChuanQiu <lisi806@163.com>
2013-07-09 14:00:10 +0800

    add base64 and md5

--- src/接口/Aclass.java
+++ src/接口/Aclass.java
@@ -13,4 +13,10 @@ public class Aclass extends Aabstract{
 	public void play(){
 		System.out.println("Aclass playing¡­¡­");
 	}
+
+	@Override
+	public void a() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/接口/inner/Aabstract.java
+++ src/接口/inner/Aabstract.java
@@ -1,5 +1,5 @@
 package ½Ó¿Ú.inner;
 
 public abstract class Aabstract {
-    abstract void a();
+    public abstract void a();
 }
